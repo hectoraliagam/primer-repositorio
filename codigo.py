53022fb57ad6ed99405a3889293a0eed19eb78d8
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2024-05-28 18:34:58 -0500

    Subiendo los archivos de todo el curso de git

---
+++ codigo.py
@@ -0,0 +1,4 @@
+nombre = "Hector"
+apellido = "Aliaga"
+saludo = f"Mi nombre es {nombre} {apellido}, como tan muchacho?? xd"
+print(saludo)
---
+++ func.py
@@ -0,0 +1,3 @@
+def suma(*num):
+    return sum(num)
+print(suma(2,3,4,7,8,56,8,94,4))
