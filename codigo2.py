8166198cf9498be23c85f05f75da70985e0c11a2
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2024-05-28 19:40:23 -0500

    Update codigo2.py

--- codigo2.py
+++ codigo2.py
@@ -1,4 +1,5 @@
 nombre = "zzz"
 print(nombre)
-name = input(f"pasa tu nombre real {nombre})
-print("por fin")
+name = input(f"pasa tu nombre real {nombre}: ")
+print(f"tu nombre real es {name}, supongo")
+print("hize otro de nuevo :,v")
